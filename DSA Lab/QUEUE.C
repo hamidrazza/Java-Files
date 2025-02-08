24550aa004e7f90ece7303d7d5abdbe7589f8180
hamidrazza <razzahamid03@gmail.com>
2025-02-09 00:25:56 +0530

    Beautified the code

--- DSA Lab/QUEUE.C
+++ DSA Lab/QUEUE.C
@@ -9,10 +9,12 @@ void enque();
 void deque();
 void display();
 
-int repeat=1, choice;
-int main(){
-	//clrscr(); // Using this because of the Turbo C.
-	while(repeat){
+int repeat = 1, choice;
+int main()
+{
+	// clrscr(); // Using this because of the Turbo C.
+	while (repeat)
+	{
 		printf("Choose an option\n");
 		printf("1. Insert\n");
 		printf("2. Delete\n");
@@ -21,76 +23,91 @@ int main(){
 		printf("5. Display\n");
 
 		scanf("%d", &choice);
-		switch(choice){
-			case 1:
-				enque();
-				//clrscr(); // clearing every time while calling the function.
-				break;
-			case 2:
-				deque();
-				//clrscr();
-				break;
-			case 3:
-				printf("\nFront element is: %d",queue[front]);
-				break;
-			case 4:
-				printf("\nRear element is: %d",queue[rear]);
-				break;
-			case 5:
-				display();
-				break;
-			default:
-				printf("\nInvalid input\n");
+		switch (choice)
+		{
+		case 1:
+			enque();
+			// clrscr(); // clearing every time while calling the function.
+			break;
+		case 2:
+			deque();
+			// clrscr();
+			break;
+		case 3:
+			printf("\nFront element is: %d", queue[front]);
+			break;
+		case 4:
+			printf("\nRear element is: %d", queue[rear]);
+			break;
+		case 5:
+			display();
+			break;
+		default:
+			printf("\nInvalid input\n");
 		}
 		printf("\nWant to continue 0/1 ");
 		scanf("%d", &repeat);
 	}
 	return 0;
 }
-void enque(){
-	if(rear == capacity-1){
+void enque()
+{
+	if (rear == capacity - 1)
+	{
 		printf("\nQueue is Full\n");
 	}
-	else{
+	else
+	{
 		rear++;
 		printf("\nEnter the no: ");
 		scanf("%d", &queue[rear]);
-		if(front == -1){
+		if (front == -1)
+		{
 			front++;
 		}
-		printf("%d is inserted successfully\n",queue[rear]);
+		printf("%d is inserted successfully\n", queue[rear]);
 	}
 }
-void deque(){
-	if(rear == -1){
+void deque()
+{
+	if (rear == -1)
+	{
 		printf("\nQueue is empty\n");
 	}
-	else{
-		printf("\n%d is deleted\n",queue[front]);
+	else
+	{
+		printf("\n%d is deleted\n", queue[front]);
 	}
-	if(rear == 0 && front == 0){
+	if (rear == 0 && front == 0)
+	{
 		queue[front] = NULL;
 		front--;
 		rear--;
 	}
-	else{
+	else
+	{
 		queue[front] = NULL;
 		int i;
-		for(i=0; i<rear;i++){
-			queue[i] = queue[i+1];
+		for (i = 0; i < rear; i++)
+		{
+			queue[i] = queue[i + 1];
 		}
 		rear--;
 	}
 }
-void display(){
-	if(rear == -1){
+void display()
+{
+	if (rear == -1)
+	{
 		printf("\nQueue is empty\n");
 	}
-	else{
+	else
+	{
 		int i;
 		printf("\nQueue elements are : ");
-		for(i=0; i <= rear; i++){
-			printf("%d\t",queue[i]);
+		for (i = 0; i <= rear; i++)
+		{
+			printf("%d\t", queue[i]);
 		}
 		printf("\n");
 	}
--- DSA Lab/STACKINC.C
+++ DSA Lab/STACKINC.C
@@ -2,69 +2,82 @@
 #include <conio.h>
 
 int top = -1;
-int capacity=10;
+int capacity = 10;
 int stack[10];
 void push();
 void pop();
 void display();
 
-void main(){
+void main()
+{
 
 	int repeat = 1, choice;
-	while(repeat){
-	printf("1. push\n");
-	printf("2. pop\n");
-	printf("3. Display\n");
-	scanf("%d", &choice);
- switch(choice){
- case 1:
-	clrscr();
-	push();
-	break;
- case 2:
-	clrscr();
-	pop();
-	break;
- case 3:
-	clrscr();
-	display();
-	break;
- default:
-	clrscr();
-	printf("Please enter the valid option\n");
-	break;
- }
- printf("Enter 1 to continue, OR 0 for exit ");
- scanf("%d", &repeat);
- }
- }
- void push(){
-	if(top == capacity-1){
-	 printf("Stack is Full");
+	while (repeat)
+	{
+		printf("1. push\n");
+		printf("2. pop\n");
+		printf("3. Display\n");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			//clrscr();
+			push();
+			break;
+		case 2:
+			//clrscr();
+			pop();
+			break;
+		case 3:
+			//clrscr();
+			display();
+			break;
+		default:
+			//clrscr();
+			printf("Please enter the valid option\n");
+			break;
+		}
+		printf("Enter 1 to continue, OR 0 for exit ");
+		scanf("%d", &repeat);
 	}
-	else{
-	 top++;
-	 printf("Enter the element: ");
-	 scanf("%d", &stack[top]);
+}
+void push()
+{
+	if (top == capacity - 1)
+	{
+		printf("Stack is Full");
 	}
- }
- void pop(){
-	if(top == -1){
-	 printf("Stack is empty");
+	else
+	{
+		top++;
+		printf("Enter the element: ");
+		scanf("%d", &stack[top]);
 	}
-	else{
-	 printf("Popped element is : %d\n", stack[top]);
+}
+void pop()
+{
+	if (top == -1)
+	{
+		printf("Stack is empty");
 	}
- }
- void display(){
+	else
+	{
+		printf("Popped element is : %d\n", stack[top]);
+	}
+}
+void display()
+{
 	int i;
-	if(top == -1){
-	 printf("Stack is empty");
+	if (top == -1)
+	{
+		printf("Stack is empty");
 	}
-	else{
-	 for(i = top; i >= 0; i--){
-	  printf("%d ", stack[i]);
-	 }
-	 printf("\n");
+	else
+	{
+		for (i = top; i >= 0; i--)
+		{
+			printf("%d ", stack[i]);
+		}
+		printf("\n");
 	}
- }
+}
