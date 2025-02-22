047c2dd42c2dd2660c4a941f44915eae5aca7ba7
hamidrazza <razzahamid03@gmail.com>
2025-02-22 16:31:57 +0530

    Banking Program

--- Assignment/JavaTask/src/PerToCGPA.java
+++ Assignment/JavaTask/src/PerToCGPA.java
@@ -1,28 +1,33 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-class PerToCGPA{
-	public static void main(String[] args){
+class PerToCGPA {
+	public static void main(String[] args) {
 		System.out.println("=== Program to convert Percentage to CGPA ===");
 		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("#.00");
 
 		LocalDateTime now = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm");
 		String dateTime = now.format(formatter);
 
 		System.out.println("Date : " + dateTime);
+
 		// Checking if the percentage is exceeding 100 or not.
-		System.out.print("How many subjects do you have : ");
+		System.out.print("How many subjects do you have: ");
 		int subjects = sc.nextInt();
-		double marks = 0;
+		double marks = 0.0; // Directly use primitive types
 		for (int i = 1; i <= subjects; i++) {
-			System.out.print("Enter your subject " + i + " marks : ");
+			System.out.print("Enter your subject " + i + " marks: ");
 			double number = sc.nextDouble();
 			marks += number;
 		}
-		double percentage =  marks / subjects;
-		double cgpa = percentage/9.5;
-		System.out.println("Your current percentage is " + String.format("%.2f",percentage) + " and CGPA is " + String.format("%.2f",cgpa));
+		double percentage = marks / subjects;
+		double cgpa = percentage / 9.5;
+
+		// Use DecimalFormat to format percentage and cgpa
+		System.out.println("Your current percentage is " + df.format(percentage) + " and CGPA is " + df.format(cgpa));
 	}
 }
